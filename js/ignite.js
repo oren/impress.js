a9273237cf64dd2f2a594744ec3b015145b8c2bd
Oren <ogolan@attinteractive.com>
2012-03-31 04:00:05 -0700

    countdown timer for each slide

--- js/ignite.js
+++ js/ignite.js
@@ -1,14 +1,33 @@
 (function() {
 
   var index = 1;
+  var time = 14;
+  var times = $('.time');
 
   function start() {
+    restartTimer();
     var intervalID = setInterval(function() {
       index += 1;
       impress().next();
+      restartTimer();
 
       if(index === 20) { window.clearInterval(intervalID) };
     }, 15000);
+  };
+
+  function restartTimer() {
+    var intervalID2 = setInterval(function() {
+      var step = $('.step:nth-child(' + index + ')');
+      var timeElem = step.children('.time');
+      times.hide();
+      timeElem.show();
+      timeElem.text(time);
+      time -= 1;
+      if(time === 0) { 
+        window.clearInterval(intervalID2) 
+        time = 14;
+      };
+    }, 1000);
   };
 
   Ignite =  {
